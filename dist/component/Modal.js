cf435128d3af9a6ad24bd5ed1afdea10ddc161f3
Eliaexe <eliasidori@outllok.it>
2023-08-11 14:55:13 +0200

    npm package v1 is ok

---
+++ dist/component/Modal.js
@@ -0,0 +1,92 @@
+"use strict";
+
+require("core-js/modules/es.symbol.description.js");
+require("core-js/modules/es.weak-map.js");
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = Modal;
+require("core-js/modules/web.dom-collections.iterator.js");
+var _react = _interopRequireWildcard(require("react"));
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { _defineProperty(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
+function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return typeof key === "symbol" ? key : String(key); }
+function _toPrimitive(input, hint) { if (typeof input !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (typeof res !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
+// Modal component that displays a customizable modal
+function Modal(props) {
+  // State to manage the modal's open state
+  const [isModalOpen, setIsModalOpen] = (0, _react.useState)(props.open);
+
+  // Effect to update the modal's state when 'open' prop changes
+  (0, _react.useEffect)(() => {
+    setIsModalOpen(props.open);
+  }, [props.open]);
+
+  // Function to close the modal and call onCloseModal prop
+  const closeModal = () => {
+    setIsModalOpen(false);
+    props.onCloseModal();
+  };
+
+  // Default style for the modal container, can be customized with props
+  const defaultModalStyle = _objectSpread({
+    position: 'relative',
+    padding: '20px',
+    borderRadius: '8px',
+    boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.2)',
+    maxWidth: '80%',
+    zIndex: 1001
+  }, props.containerStyle);
+
+  // Style for the overlay that darkens the background when modal is open
+  const overlayStyle = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    display: isModalOpen ? 'flex' : 'none',
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 1000
+  };
+
+  // Style for the close button inside the modal
+  const closeButtonStyle = _objectSpread({
+    position: 'absolute',
+    top: '-10px',
+    right: '-10px',
+    cursor: 'pointer',
+    border: 'none',
+    borderRadius: '50%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+    color: 'white',
+    width: '30px',
+    height: '30px'
+  }, props.closeButtonStyle);
+
+  // Style for the message inside the modal, can be customized with props
+  const messageStyle = _objectSpread({}, props.messageStyle);
+
+  // Render the modal with customized styles and content
+  return /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("div", {
+    className: "overlay",
+    style: overlayStyle
+  }, /*#__PURE__*/_react.default.createElement("div", {
+    className: "modale",
+    style: defaultModalStyle
+  }, /*#__PURE__*/_react.default.createElement("button", {
+    className: "close-button",
+    style: closeButtonStyle,
+    onClick: closeModal
+  }, "X"), /*#__PURE__*/_react.default.createElement("h3", {
+    style: messageStyle
+  }, props.message))));
+}
---
+++ src/lib/component/Modal.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from 'react';
+
+// Modal component that displays a customizable modal
+export default function Modal(props) {
+  // State to manage the modal's open state
+  const [isModalOpen, setIsModalOpen] = useState(props.open);
+
+  // Effect to update the modal's state when 'open' prop changes
+  useEffect(() => {
+    setIsModalOpen(props.open); 
+  }, [props.open]);
+
+  // Function to close the modal and call onCloseModal prop
+  const closeModal = () => {
+    setIsModalOpen(false);
+    props.onCloseModal();
+  };
+
+  // Default style for the modal container, can be customized with props
+  const defaultModalStyle = {
+    position: 'relative',
+    padding: '20px',
+    borderRadius: '8px',
+    boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.2)',
+    maxWidth: '80%',
+    zIndex: 1001,
+    ...props.containerStyle 
+  };
+
+  // Style for the overlay that darkens the background when modal is open
+  const overlayStyle = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    display: isModalOpen ? 'flex' : 'none',
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 1000
+  };
+
+  // Style for the close button inside the modal
+  const closeButtonStyle = {
+    position: 'absolute',
+    top: '-10px',
+    right: '-10px',
+    cursor: 'pointer',
+    border: 'none',
+    borderRadius: '50%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+    color: 'white',
+    width: '30px',
+    height: '30px',
+    ...props.closeButtonStyle 
+  };
+
+  // Style for the message inside the modal, can be customized with props
+  const messageStyle = {
+    ...props.messageStyle
+  }
+
+  // Render the modal with customized styles and content
+  return (
+    <div>
+      <div className="overlay" style={overlayStyle}>
+        <div className="modale" style={defaultModalStyle}>
+          <button className="close-button" style={closeButtonStyle} onClick={closeModal}>
+            X
+          </button>
+          <h3 style={messageStyle}>{props.message}</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
